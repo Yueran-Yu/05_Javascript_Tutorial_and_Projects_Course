998b326c394f7d8972800b13cb8f46126e07d540
GraceYu <librayuyueran@gmail.com>
2020-11-23 23:28:37 -0600

    Use Chuck Norris API

--- AJAX_XHR_Promise/1-jokes-setup/js/app.js
+++ AJAX_XHR_Promise/1-jokes-setup/js/app.js
@@ -1,26 +1,24 @@
-//
-const mainBtn = document.getElementById("mainBtn");
-const result = document.getElementById("result");
-const imgContainer = document.getElementById("img");
+const mainBtn = document.querySelector('#mainBtn')
+const result = document.querySelector('#result')
+const imgContainer = document.querySelector('#img')
 
-mainBtn.addEventListener("click", function() {
-  const ajax = new XMLHttpRequest();
-  const url = "https://api.chucknorris.io/jokes/random";
-  ajax.open("GET", url, true);
 
-  ajax.onload = function() {
+mainBtn.addEventListener('click', function () {
+  const ajax = new XMLHttpRequest()
+  const url = "https://api.chucknorris.io/jokes/random"
+  ajax.open('GET', url, true)
+
+  ajax.onload = function () {
     if (this.status === 200) {
-      const data = JSON.parse(this.responseText);
-      const { icon_url: img, value: joke } = data;
+      const data = JSON.parse(this.responseText)
+      const { icon_url: img, value: joke } = data
 
-      result.textContent = joke;
-      imgContainer.src = img;
+      result.textContent = joke
+      imgContainer.src = img
     }
-  };
-
-  ajax.onerror = function() {
-    console.log("there was an erro");
-  };
-
-  ajax.send();
-});
+  }
+  ajax.onerror = function () {
+    console.log('there was an errors');
+  }
+  ajax.send()
+})
