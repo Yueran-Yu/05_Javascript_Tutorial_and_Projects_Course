277a5e5d57345808c14948ffb0d6354080a918c9
GraceYu <librayuyueran@gmail.com>
2020-11-25 19:38:12 -0600

    Fetch API

--- AJAX_XHR_Promise/2-randomPerson-setup/js/app.js
+++ AJAX_XHR_Promise/2-randomPerson-setup/js/app.js
@@ -1,38 +1,59 @@
 const btn = document.querySelector('#btn')
 
-btn.addEventListener('click', function(){
-  getPerson(getData)
+btn.addEventListener('click', function () {
+
+  fetch("https://randomuser.me/api/")
+    .then(data => data.json())
+    .then(data => getData(data, showData))
+    .catch(error => console.log(error))
 })
 
-function getPerson(getDataCallback){
-  const url = "https://randomuser.me/api/"
-  const method = "GET"
-  const ajax = new XMLHttpRequest()
-  ajax.open(method, url, true)
+// const ajax = new Promise((resolve, reject) => {
+//   const url = "https://randomuser.me/api/"
+//   const xhr = new XMLHttpRequest()
+//   xhr.open('GET', url, true)
 
-  ajax.onload = function(){
-    if(this.status === 200){
-      getDataCallback(this.responseText, showData)
-    }else{
+//   xhr.onload = () => {
+//     if (xhr.status === 200) {
+//       resolve(xhr.responseText)
+//     } else {
+//       reject(Error(xhr.statusText))
+//     }
+//   }
+//   xhr.onerror = () => {
+//     reject(Error('there was an error'))
+//   }
+//   xhr.send()
+// })
 
-    }
-  }
-  ajax.onerror = function(){
-    console.log("get data");
-  }
-  ajax.send()
-}
+// function getPerson(getDataCallback) {
+//   const url = "https://randomuser.me/api/"
+//   const method = "GET"
+//   const ajax = new XMLHttpRequest()
+//   ajax.open(method, url, true)
 
-function getData(responseText, showDataCallBack){
-  const data = JSON.parse(responseText)
+//   ajax.onload = function () {
+//     if (this.status === 200) {
+//       getDataCallback(this.responseText, showData)
+//     } else {
 
-  const {name:{first}, name:{last}, picture:{large}, location: {street:{number: sNumber}, street:{name: sName}}, phone, email} = data.results[0]
+//     }
+//   }
+//   ajax.onerror = function () {
+//     console.log("here is an error")
+//   }
+//   ajax.send()
+// }
 
+function getData(responseText, showDataCallBack) {
+  // const data = JSON.parse(responseText)
+
+  const { name: { first }, name: { last }, picture: { large }, location: { street: { number: sNumber }, street: { name: sName } }, phone, email } = responseText.results[0]
 
   showDataCallBack(first, last, large, sNumber, sName, phone, email)
 }
 
-function showData(first, last, large, sNumber, sName, phone, email){
+function showData(first, last, large, sNumber, sName, phone, email) {
   document.querySelector('#first').textContent = first
   document.querySelector('#last').textContent = last
   document.querySelector('#photo').src = large
@@ -41,3 +62,19 @@ function showData(first, last, large, sNumber, sName, phone, email){
   document.querySelector('#email').textContent = email
   console.log(first, last, large, street, phone, email);
 }
+
+// fetch("https://randomuser.me/api").then(data => data.json()).then(data => console.log(data)).catch(error => console.log(error))
+
+
+// function externalData(url) {
+//   // new Promise object, pass the callback function
+//   return new Promise(function(resolve, reject) {
+//     if (url.length > 0) {
+//       resolve("response data.")
+//     } else {
+//       reject("there was an error.")
+//     }
+//   })
+// }
+
+// externalData("url").then(data => console.log(data)).catch(error => console.log(error))
